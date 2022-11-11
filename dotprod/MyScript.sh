ebec33410f0b5638ad397a77fc7ab69ef981a91f
Leila IMANI <leila.imani@ens.uvsq.fr>
2022-11-11 20:15:29 +0100

    Modification des scipte bash
    Ajout du chois du flage d'optimisation.
    Et corection des scripte de dotprod et reduc.

--- dotprod/MyScript.sh
+++ dotprod/MyScript.sh
@@ -8,27 +8,13 @@ echo "# Création de répertoires: Mesure"
 #
 echo "Entrer le compilateur :"
 read cc
-make CC=$cc
+echo "Entrer le flags d'optimisation :"
+read Oflags
+make CC=$cc OFLAGS=$Oflags
 
 #
 echo "Entrer n :"
 read n
 echo "Entrer r :"
 read r
-./dgemm $n $r > "Mesure/$n-x-$r-$cc.dat"
-cd "Mesure"
-
-#
-mkdir -p "$n-x-$r-$cc"
-echo "# Création de répertoires: Mesure/$n-x-$r-$cc"
-
-#
-nb=$( wc -l "$n-x-$r-$cc.dat"  | cut -d' ' -f1 )
-
-#
-for (( i=1; i < $nb; i++ ));
-do
-    head -n 1 "$n-x-$r-$cc.dat" > "$n-x-$r-$cc/$i-$cc.dat"
-    echo "$(tail -n "$i" "$n-x-$r-$cc.dat" | head -n 1)" | awk '{gsub(/;/,"")}1'  >> "$n-x-$r-$cc/$i-$cc.dat"
-    echo "# Création du fichier: Mesure/$n-x-$r-$cc/$i-$cc.dat"
-done
+./dotprod $n $r > "Mesure/$n-x-$r-$cc$Oflags.dat"
